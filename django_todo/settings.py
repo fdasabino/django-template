3de46d2d0fd4d8c5f74513a9b7d123ac5f33c1d1
Francisco Sabino <fdasabino@gmail.com>
2021-08-14 19:00:24 +0200

    fixed allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3jgu@zspy&zdlz1sj#n9wohe&np1ksqcsi@l$djy*hq^s*(q3#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['frank-django.herokuapp.com']
 
 
 # Application definition
